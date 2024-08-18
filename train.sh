9facede1fff3690e9e2e7bc9dec184287c94d864
wangqixiong <wangqixiong@xiaohongshu.com>
2024-08-18 18:51:49 +0800

    de

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=3,4,5,6 python -m torch.distributed.launch --nproc_per_node 4 \
+CUDA_VISIBLE_DEVICES=3,4,5,6 python -m torch.distributed.launch --nproc_per_node 1 \
 --master_port 12345 train.py --model lavt  --batch-size 8 --lr 0.00005 --wd 1e-2 \
 --swin_type base --pretrained_swin_weights ./pretrained_weights/swin_base_patch4_window12_384_22k.pth \
 --epochs 40 --img_size 480 \
